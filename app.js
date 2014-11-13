1bd21d6d3dc19526c9b32ada1a2734b476d78e5b
teambraison <tituscheng@flor3nce.local>
2014-11-13 11:09:29 +0900

    Added new api calls

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express")
+var session = require('express-session')
 var swig = require("swig")
 var app = express()
 var bodyParser = require("body-parser")
@@ -17,8 +18,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 //app.use(express.json())
 //app.use(express.urlencoded())
 
+app.use(session({secret:"123fantastic",
+                 saveUninitialized: true,
+                 resave: true}))
 
-mongoose.connect("mongodb://127.0.0.1/DOT")
+
+//mongoose.connect("mongodb://127.0.0.1/DOT")
+
+mongoose.connect("mongodb://nodejitsu:36026df69744f52ce9a94aa3673d78fd@troup.mongohq.com:10037/nodejitsudb3283738885")
 
 var db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error: "))
@@ -33,6 +40,9 @@ var User = mongoose.model("User", userSchema)
 var messageSchema = mongoose.Schema({to:String, from: String, message: String, status:String, date: Date})
 var Message = mongoose.model("Message", messageSchema)
 
+var sessionSchema = mongoose.Schema({session_id: String, user_id: String, username: String})
+var Session = mongoose.model("Session", sessionSchema)
+
 var ID = function () {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
@@ -41,7 +51,19 @@ var ID = function () {
 }
 
 app.get('/', function(req, res) {
-    res.send(swig.renderFile(indexPagePath, {title: "DOT"}))
+    Session.find({session_id:req.session.session_id}, function(err, data) {
+        console.lg
+        console.log("Data: " + "\"" + data + "\"")
+        if(err) {
+            console.log(err)
+        } 
+        if(data == ""){
+           res.send(swig.renderFile(indexPagePath, {title: "DOT"}))
+        }
+        else {
+            res.send(swig.renderFile(indexPagePath, {title: "DOT", welcome_message: "Welcome back " + data[0].username}))
+        }
+    })
 })
 
 app.get('/signupform', function(req, res) {
@@ -70,8 +92,22 @@ app.post('/api/adduser', function(req,res){
     })
 })
 
+function checkAuth(req, res, next) {
+    if(!req.session.user_id) {
+        res.send("You are not authorized to view this page")
+    } else {
+        next();
+    }
+}
+
+
+
 app.get('/loginform', function(req, res){
     res.send(swig.renderFile(loginPagePath, {title: "Login In Page"}))
+})
+
+app.get('/loginsuccess', function(req, res){
+    res.send("You have successfully logged in")
 })
 
 app.post('/api/login', function(req, res) {
@@ -83,11 +119,25 @@ app.post('/api/login', function(req, res) {
             if(data == "") {
                 res.send("Found no user " + req.body.user)
             } else {
-                res.send("User authenticated - authorize access")
+                if(!req.session.session_id) {
+                    var sessionid = ID()
+                    var newsession = new Session({session_id: sessionid, user_id:data[0].id, username:data[0].username})
+                    newsession.save()
+                    req.session.session_id = sessionid
+                    req.session.user_id = user_data[0].id
+                }
+                res.send("user authenticated")
             }
         }
     })
 })
+
+//app.post('/api/messages/send', function(req, res){
+//    if(Session.find({session_id:req.session.session_id, user_id:req.session.user_id}).count())
+//    {
+//        
+//    }
+//})
 
 app.get('/api/allusers', function(req, res) {
     User.find({}, {username:1}, function(err, data) {
@@ -95,11 +145,14 @@ app.get('/api/allusers', function(req, res) {
             console.log(err)
             res.send("Found no users in our database")
         } else {
-            var usernames = []
+            var namesObject = {}
+            var names = []
+            
+            namesObject.names = names
             for(var i = 0; i < data.length; i++) {
-                usernames.push(data[i].username)
+                namesObject.names.push(data[i].username)
             }
-            res.send(usernames.join())
+            res.send(namesObject)
         }
     })
 
